cef9855019df73e1a2dc51cf3e4cf6ccddffcb80
Casonka <alexandrnazarovfalloutgame4ch@gmail.com>
2023-01-10 16:39:04 +0300

    work with uros app(#2)

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -59,7 +59,10 @@ PID Regulator[2];
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+const char *subscriber_name = "TransmissionPosition";
+const char *publisher_name = "CarStateResponse";
+const int timeout_ms = 250;
+const uint8_t attempts = 5;
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -71,7 +74,7 @@ PID Regulator[2];
 /* USER CODE BEGIN Variables */
   std_msgs__msg__Int8 msgTransmission;
   diagnostic_msgs__msg__DiagnosticArray msgDiagnostic;
-
+  rcl_ret_t ret;
 /* USER CODE END Variables */
 /* Definitions for xuROS */
 osThreadId_t xuROSHandle;
@@ -112,7 +115,7 @@ const osMessageQueueAttr_t xStateQueue_attributes = {
 /* Definitions for xRegulator */
 osTimerId_t xRegulatorHandle;
 const osTimerAttr_t xRegulator_attributes = {
-  .name = "xRegulator"
+  .name = "xRegulator",
 };
 
 
@@ -222,6 +225,18 @@ void   microros_deallocate(void * pointer, void * state);
 void * microros_reallocate(void * pointer, size_t size, void * state);
 void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state);
 
+void ConstructAnswer(char* message, uint32_t millisec, uint32_t nanosec) {
+	msgDiagnostic.header.stamp.nanosec = nanosec;
+	msgDiagnostic.header.stamp.sec = millisec / 1000;
+	msgDiagnostic.header.frame_id.data = message;
+}
+
+void subscription_callback(const void* msgin) {
+	const std_msgs__msg__Int8 * msg = (std_msgs__msg__Int8 *)msgin;
+	msgTransmission.data = msg->data;
+	printf("Received msg from topic \r\n");
+}
+
 void uROSTask(void *argument)
 {
   /* USER CODE BEGIN uROSTask */
@@ -244,9 +259,9 @@ void uROSTask(void *argument)
 	  }
 
 	  // micro-ROS app
-
 	  rcl_publisher_t publisher;
-	  std_msgs__msg__Int8 msg;
+	  rcl_subscription_t subscriber;
+
 	  rclc_support_t support;
 	  rcl_allocator_t allocator;
 	  rcl_node_t node;
@@ -257,28 +272,58 @@ void uROSTask(void *argument)
 	  rclc_support_init(&support, 0, NULL, &allocator);
 
 	  // create node
-	  rclc_node_init_default(&node, "cubemx_node", "", &support);
+	  rclc_node_init_default(&node, "TransmissionModule", "", &support);
 
-	  rclc_publisher_init_default(
+	  ret = rclc_publisher_init_default(
 	    &publisher,
 	    &node,
-	    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int8),
-	    "cubemx_publisher");
-	  // 100 Hz timer
-	  osTimerStart(xRegulatorHandle, (10/portTICK_RATE_MS));
+	    ROSIDL_GET_MSG_TYPE_SUPPORT(diagnostic_msgs, msg, DiagnosticArray),
+	    publisher_name);
+
+	  if(RCL_RET_OK != ret) {
+		  // RESET TASK
+	  }
+
+	  ret = rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int8),
+		subscriber_name);
+
+	  rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+	  ret = rclc_executor_init(&executor, &support.context, 1, &allocator);
+
+	  ret = rclc_executor_add_subscription(&executor, &subscriber, &msgDiagnostic,
+			  &subscription_callback, ON_NEW_DATA);
+	  if(RCL_RET_OK != ret) { /* RESET TASK */ }
+
+	  int64_t time_ns, time_ms = 0;
 
   /* Infinite loop */
   for(;;)
   {
-	    rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
-	    if (ret != RCL_RET_OK)
-	    {
-	      printf("Error publishing (line %d)\n", __LINE__);
-	    }
+	  	rmw_ret_t ping_result = rmw_uros_ping_agent(timeout_ms,attempts);
+	  	if(RMW_RET_OK != ping_result) { /* RESET TASK */ }
 
-	    //msg.data++;
+	  	rmw_uros_sync_session(timeout_ms);
+	  	if(!rmw_uros_epoch_synchronized()) { /* RESET TASK */ }
+	  	time_ms = rmw_uros_epoch_millis();
+	  	time_ns = rmw_uros_epoch_nanos();
+
+
+	  	ConstructAnswer("OK",time_ms, time_ns);
+	  	ret = rcl_publish(&publisher, &msgDiagnostic, NULL);
+
+
+	  	if(RCL_RET_OK != ret) { /* RESET TASK */ }
+	  	rclc_executor_spin_some(&executor, RCL_MS_TO_NS(50));
+
 	    osDelay(10);
   }
+
+  ret = rcl_publisher_fini(&publisher,&node);
+  ret = rcl_subscription_fini(&subscriber,&node);
+  vTaskDelete(NULL);
   /* USER CODE END uROSTask */
 }
 
@@ -318,10 +363,12 @@ void RestartTask(void *argument)
   /* USER CODE END RestartTask */
 }
 
-void ParseEncoderData(void) {
 
-}
 /* Regulator_Callback function */
+/*!
+ * Программный таймер управления регулятором
+ * @param argument
+ */
 void Regulator_Callback(void *argument)
 {
   /* USER CODE BEGIN Regulator_Callback */
